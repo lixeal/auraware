99003be120c9d878ff22f83142c48e56e84caa1a
kaspyX <bogdan.sumskoy@gmail.com>
2026-01-26 19:20:10 +0200

    Create index.js

---
+++ auraware/api/index.js
@@ -0,0 +1,16 @@
+// api/index.js
+export default async function handler(req, res) {
+  const GITHUB_URL = 'https://raw.githubusercontent.com/lixeal/xllr/refs/heads/home/walk.lua';
+  
+  try {
+    const response = await fetch(GITHUB_URL);
+    if (!response.ok) throw new Error('GitHub error');
+    
+    const luaScript = await response.text();
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    return res.status(200).send(luaScript);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('-- Error loading script');
+  }
+}
